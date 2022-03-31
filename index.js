0f4574f8871891e7721ea65921f2d06477b7aa7e
Felipe Belisario <felipesantos.dsb@hotmail.com>
2022-03-31 20:02:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,39 +2,40 @@
 //Por: Felipe dos Santos Belisário
 //Referência: https://github.com/valtech-br/vcamp-edabit-instrucoes
 
-/* Função adiciona elemento, por meio de um For Loop itera de 0 até o tamanho desejado, então pega o número inicial e soma na variável 'soma'
-esse resultado armazena no array 'somaResultado', assim para cada laço os números contidos no array somaResultado vai pulando de acordo com o numInicial, ou seja
-se numInicial == 1, o array vai ser [1,2,3,4,5,n], se numInicial == 2 o array somaResultado vai ser: [2,4,6,8,10,n] assim sucessivamente.
-
+/* Função calculaElemento, tem o objetivo de calcular o valor que será alocado em cada índice do array listaResultado.
+Assim para cada iteração os números contidos no array seguirão em sequencia de acordo com o numInicial.
+Por exemplo: Se numInicial == 1, o array vai ser listaResultado = [1,2,3,4,5,n], se numInicial == 2 o array  vai ser listaResultado = [2,4,6,8,10,n] assim sucessivamente.
 */
-function adicionaElemento(numInicial, tamanho) {
-  let somaResultado = []
+
+function calculaElemento(numInicial, tamanho) {
+  let listaResultado = []
   let soma = 0;
   for (let i = 0; i < tamanho; i++) {
     soma = soma + numInicial;
-    somaResultado.push(soma);
+    listaResultado.push(soma);
   }
-  return somaResultado;
+  return listaResultado;
 }
 
 /* 
-Função multiplicationTable usada pra passar na função adicionalElemento o valor (n) vezes no total que esta função vai rodar e também pra determinar o valor inicial j
+Função multiplicationTable responsável por montar a tabela de multiplicação. A regra do cálculo é realizada na função calculaElemento.
 O For loop vai de 1 até n fazendo com que o resultado final seja (n) arrays em sequência, formando o resultado final do exercício, por exemplo:
 multiplicationTable(3)
 output:
 [1,2,3]
 [2,4,6]
 [3,6,9]
-
 */
 
 function multiplicationTable(n) {
 	let resultado = [];
     for (let j = 1; j <= n; j++) {
-      resultado.push(adicionaElemento(j, n));
+      resultado.push(calculaElemento(j, n));
     }
     return resultado;
   }
+
+
 
 
 
